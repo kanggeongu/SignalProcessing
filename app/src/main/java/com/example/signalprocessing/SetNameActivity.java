6d1f4d71b16a82b537cb27afcd7e36baca356fc8
eonju <deer8203@naver.com>
2020-05-05 16:03:54 +0900

    2020-05-05 로그인 기능

---
+++ app/src/main/java/com/example/signalprocessing/SetNameActivity.java
@@ -0,0 +1,14 @@
+package com.example.signalprocessing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SetNameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_name);
+    }
+}
