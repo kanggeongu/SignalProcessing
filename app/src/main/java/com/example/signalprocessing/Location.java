97cb0fbe4c092735205fb91d381595eb6db06011
ByungJu <51966292+juu10326@users.noreply.github.com>
2020-06-13 22:01:12 +0900

    게시판 검색

---
+++ app/src/main/java/com/example/signalprocessing/Location.java
@@ -0,0 +1,49 @@
+package com.example.signalprocessing;
+
+import java.io.Serializable;
+
+public class Location implements Serializable {
+    private float x;
+    private float y;
+    private String userName;
+    private String content;
+
+    Location(){
+        x=0;
+        y=0;
+        userName="";
+        content="";
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ app/src/main/java/com/example/signalprocessing/SearchActivity.java
@@ -0,0 +1,102 @@
+package com.example.signalprocessing;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SearchActivity extends AppCompatActivity {
+
+    private EditText editSearch;
+    private Button SearchButton;
+    private Context mContext;
+
+    private RecyclerView recyclerView;
+    private ArticleAdapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    FirebaseDatabase Database = FirebaseDatabase.getInstance();
+    DatabaseReference mDatabaseReference = Database.getReference();
+
+    public User user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        editSearch=(EditText)findViewById(R.id.editSearch);
+        SearchButton=(Button)findViewById(R.id.searchButton);
+        recyclerView=(RecyclerView)findViewById(R.id.searchRecycler);
+        mContext=this;
+
+        user=(User)getIntent().getSerializableExtra("userInformation");
+        final ArrayList<Article> articleList = new ArrayList<>();
+
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(mContext);
+        recyclerView.setLayoutManager(layoutManager);
+        mAdapter = new ArticleAdapter(articleList);
+
+        mDatabaseReference.child("Articles").child(user.getUserUniv()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    Article article = postSnapshot.getValue(Article.class);
+                    articleList.add(article);
+                }
+                Collections.reverse(articleList);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        SearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String search=editSearch.getText().toString();
+                if(search.length()<=1){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                    builder.setTitle("").setMessage("2글자 이상 입력해주세요");
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener(){
+                        @Override
+                        public void onClick(DialogInterface dialog, int id)
+                        {
+
+                        }
+                    });
+                    AlertDialog alertDialog = builder.create();
+                    alertDialog.show();
+                }
+                else {
+                    recyclerView.setAdapter(mAdapter);
+                    CharSequence cs=search;
+                    mAdapter.getFilter().filter(cs);
+                }
+            }
+        });
+    }
+}
