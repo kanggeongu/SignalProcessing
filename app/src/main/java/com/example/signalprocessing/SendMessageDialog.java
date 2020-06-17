7bef7a5aeb02e162d78c7cc365fb0da10f0c1690
kanggeongu <48575845+kanggeongu@users.noreply.github.com>
2020-06-17 21:03:58 +0900

    6/17 func_tie_up (3_2)

--- app/src/main/java/com/example/signalprocessing/SendMessageDialog.java
+++ app/src/main/java/com/example/signalprocessing/SendMessageDialog.java
@@ -1,4 +1,28 @@
 package com.example.signalprocessing;
 
-public class SendMessageDialog {
+import android.app.Dialog;
+import android.content.Context;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+public class SendMessageDialog extends Dialog {
+
+    TextView textViewSendUser, textViewReceiveUser, textViewMessageContent;
+    Button buttonSendMessage;
+
+    public SendMessageDialog(@NonNull Context context) {
+        super(context);
+        setContentView(R.layout.activity_send_message);
+
+    }
+
+    private void initPalette() {
+        textViewSendUser = (TextView)findViewById(R.id.textViewSendUser);
+        textViewReceiveUser = (TextView)findViewById(R.id.textViewReceiveUser);
+        textViewMessageContent = (TextView)findViewById(R.id.textViewMessageContent);
+
+        buttonSendMessage = (Button)findViewById(R.id.buttonSendMessage);
+    }
 }
