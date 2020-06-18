d2c81d7c392338534e597406ed641aed0a5a77fa
kanggeongu <48575845+kanggeongu@users.noreply.github.com>
2020-06-18 23:18:40 +0900

    6/17 func_tie_up (4 - 4)

--- app/src/main/java/com/example/signalprocessing/ConnectorActivity.java
+++ app/src/main/java/com/example/signalprocessing/ConnectorActivity.java
@@ -4,12 +4,15 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -17,6 +20,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ConnectorActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -32,6 +38,7 @@ public class ConnectorActivity extends AppCompatActivity implements View.OnClick
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connector);
 
+        /*
         logout=(Button)findViewById(R.id.board_btn_logout);
         logout.setOnClickListener(this);
 
@@ -48,9 +55,62 @@ public class ConnectorActivity extends AppCompatActivity implements View.OnClick
         namecontest.setOnClickListener(this);
 
         animalbook = (Button)findViewById(R.id.board_btn_animalbook);
-        animalbook.setOnClickListener(this);
+        animalbook.setOnClickListener(this); */
 
         loadInfo(mAuth.getCurrentUser().getEmail());
+    }
+
+    private void goFreeBoard() {
+        Intent intent=new Intent(getApplicationContext(),FreeBoardActivity.class);
+        intent.putExtra("cp",0);
+        intent.putExtra("gp",0);
+        intent.putExtra("userInfo",user);
+        startActivity(intent);
+        finish();
+    }
+
+    private void checkRestricted(String userEmail) {
+        String email = userEmail.replace('.', '_');
+        mRef.child("Restricted").child("Users").child(email).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Restricted restricted = dataSnapshot.getValue(Restricted.class);
+                if (restricted == null) {
+                    goFreeBoard();
+                }
+                else {
+                    Date mDate=new Date(System.currentTimeMillis());
+                    SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyyMMdd");
+                    String date=simpleDateFormat.format(mDate);
+
+                    if(restricted.getEndDate() < Integer.parseInt(date)){
+                        goFreeBoard();
+                    }
+                    else{
+                        int year = restricted.getEndDate() / 10000;
+                        int month = (restricted.getEndDate() % 10000) / 100;
+                        int day = restricted.getEndDate() % 100;
+
+                        AlertDialog.Builder builder = new AlertDialog.Builder(ConnectorActivity.this);
+                        builder.setCancelable(false);
+                        builder.setTitle("앱 사용 금지")
+                                .setMessage(year + "년 " + month + "월 " + day + "일까지 사용 중지됩니다")
+                                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        finish();
+                                    }
+                                });
+                        builder.show();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(ConnectorActivity.this, "와이파이를 확인해주세요", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void loadInfo(final String email){
@@ -64,13 +124,15 @@ public class ConnectorActivity extends AppCompatActivity implements View.OnClick
                     User userData=snapshot.getValue(User.class);
                     if(userData.getUserEmail().equals(email)){
                         user=userData;
+
+                        checkRestricted(user.getUserEmail());
                     }
                 }
                 pdialog.dismiss();
             }
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-
+                Toast.makeText(ConnectorActivity.this, "와이파이 연결을 확인해주세요", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -90,7 +152,7 @@ public class ConnectorActivity extends AppCompatActivity implements View.OnClick
 
     @Override
     public void onClick(View view) {
-        Intent intent=null;
+        /*Intent intent=null;
         switch(view.getId()){
             case R.id.board_btn_logout:
                 signOut();
@@ -122,7 +184,7 @@ public class ConnectorActivity extends AppCompatActivity implements View.OnClick
                 intent.putExtra("userInfo", user);
                 break;
         }
-        updateUI(intent);
+        updateUI(intent);*/
     }
 
     @Override
