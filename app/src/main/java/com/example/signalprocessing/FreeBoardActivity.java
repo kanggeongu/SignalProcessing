a9643b1e349af11b04ff5e38f0efcfc0c5a146ce
eonju <deer8203@naver.com>
2020-06-19 23:32:02 +0900

    0619 수정

--- app/src/main/java/com/example/signalprocessing/FreeBoardActivity.java
+++ app/src/main/java/com/example/signalprocessing/FreeBoardActivity.java
@@ -103,13 +103,7 @@ public class FreeBoardActivity extends AppCompatActivity implements View.OnClick
         mUniv = getIntent().getStringExtra("mUniv");
         Log.e("thisgp-gp",""+thisgp);
         Log.e("thisgp-cp",""+thiscp);
-        //university= ((ConnectorActivity)ConnectorActivity.context).university;
-        university.add("경북대학교");
-        university.add("고려대학교");
-        university.add("부산대학교");
-        university.add("서울대학교");
-        university.add("연세대학교");
-        university.add("한양대학교");
+        university= ((ConnectorActivity)ConnectorActivity.context).university;
 
         Log.e("univ1",university.toString());
         int fromConnector=getIntent().getIntExtra("from",-100);
